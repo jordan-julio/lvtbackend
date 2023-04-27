fc7b70838810c20f6f7caf34a22fb6399dcfc06f
z5376268 <z5376268>
2023-04-27 21:57:57 +1000

    fix

--- src/routes/JobsScreen.jsx
+++ src/routes/JobsScreen.jsx
@@ -7,7 +7,7 @@ import { FlipCard, FlipCardBack, FlipCardFront, FlipCardInner, Title } from "../
 import PopButton from "../components/AnimatedButton";
 import FormDiv from "../components/FormDiv";
 
-export const localhost = 'https://morning-everglades-00990.herokuapp.com';
+export const localhost = 'http://morning-everglades-00990.herokuapp.com';
 function JobsScreen () {
     const [jobs, setJobs] = React.useState([]);
     const [isDataFetching, setIsDataFetching] = React.useState(false);
